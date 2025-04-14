1ca6804a6145621c86b4c7a20c4b1ccd579a0c21
Abishek Madhav <143243139+Abishek-Madhav@users.noreply.github.com>
2025-04-14 16:01:23 +0530

    Update shell.py

--- shell.py
+++ shell.py
@@ -1,10 +1,14 @@
 import abilang
 
 while True:
-    text=input('abilang > ')
-    result,error=abilang.run('<stdin>',text)
-    
-    if error:
-        print(error.as_string())
-    else:
-        print(result)
+	text = input('abilang > ')
+	if text.strip() == "": continue
+	result, error = abilang.run('<stdin>', text)
+
+	if error:
+		print(error.as_string())
+	elif result:
+		if len(result.elements) == 1:
+			print(repr(result.elements[0]))
+		else:
+			print(repr(result))
